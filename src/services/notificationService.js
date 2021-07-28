dc712687cb8adbc65818343527d0e4a43029ebbb
sankle <santi.klein@hotmail.com>
2021-07-28 18:45:26 -0300

    [ADD] notificationService removeToken

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -2,7 +2,8 @@ const { Expo } = require('expo-server-sdk');
 
 module.exports = function $notificationService(errors, logger, notificationRepository, sendNotifications) {
   return {
-    pushToken
+    pushToken,
+    removeToken
   };
 
   /**
@@ -15,5 +16,18 @@ module.exports = function $notificationService(errors, logger, notificationRepos
     if (!Expo.isExpoPushToken(token)) throw errors.create(400, `Push token ${token} is not a valid Expo push token`);
     logger.info(`Adding ExpoToken for wallet ${walletId}`);
     await notificationRepository.push(walletId, token);
+  }
+
+  /**
+   * Remove a user's expo push token
+   *
+   * @param {String} userId
+   * @param {String} token
+   * @returns {Promise}
+   */
+  async function removeToken(walletId, token) {
+    logger.info(`Removing ExpoToken for wallet ${walletId}`);
+
+    return notificationRepository.remove(walletId, token);
   }
 };
